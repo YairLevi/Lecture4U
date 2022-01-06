4b7241248acbc59fd2546f127507d62858925b4a
Yair Levi <yairlevi2001@gmail.com>
2022-01-06 16:00:39 +0200

    Boilerplate Clean-up
    Deleting all irrelevant files from the client folder

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const path = require("path")
 const express = require("express");
 const app = express(); // create express app
-
+const PORT = 8000;
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 // start express server on port 5000
-app.listen(5000, () => {
-    console.log("server started on port 5000");
+app.listen(PORT, () => {
+    console.log("server started on port 8000");
 });
