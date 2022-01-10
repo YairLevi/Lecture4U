98efa348968104db0cd8ec5d4e7d061b377958aa
Yair Levi <yairlevi2001@gmail.com>
2022-01-10 14:05:26 +0200

    #Components - Navbar
    adding a Navbar component.
    added react-bootstrap and bootstrap.
    
    +++
    
    Now can call npm start from server folder instead of from client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavbarComp/>
+            <p style={{textAlign: "center"}}>Do Something</p>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import {Container, Navbar, Nav, NavDropdown, Form, Button} from "react-bootstrap";
+
+
+export default class NavbarComp extends Component {
+    render() {
+        return (
+            <Navbar style={{padding: 15, boxShadow: '10 solid'}} bg="white" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Lecture4U</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#about">About</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <Nav.Link style={{color: 'black'}} href="#sign-up">Sign Up</Nav.Link>
+                            <Button variant="primary">Sign In</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        );
+    }
+}
