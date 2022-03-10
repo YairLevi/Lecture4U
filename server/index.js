6d16d6f1f5581d4e91700f6f2ae626f213930b7a
noam-roth <73580442+noam-roth@users.noreply.github.com>
2022-03-10 21:54:26 +0200

    executeOCR feature

--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ app.use(express.json())
 app.use('/', authRouter)
 
 const { Storage } = require('@google-cloud/storage');
+const {execSync} = require("child_process");
 
 const storage = new Storage({
     keyFilename: path.join(__dirname, 'avid-battery-339118-75042e644d3f.json')
@@ -33,17 +34,14 @@ const bucketName = 'lecture4u-1';
 app.get('/video', async (req, res) => {
     const range = req.headers.range
     if (!range) return res.status(400).send('Requires range header')
-
     const reqVideo = 'VID_20190724_211512.mp4'
     const bucket = storage.bucket(bucketName);
     const videoFile = bucket.file(reqVideo)
     const metadata = await videoFile.getMetadata()
     const videoSize = metadata[0].size
-
     const CHUNK_SIZE = 10 ** 6 // roughly 1MB
     const start = Number(range.replace(/\D/g, ""))
     const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
-
     const contentLength = end - start + 1
     const headers = {
         'Content-range': `bytes ${start}-${end}/${videoSize}`,
@@ -51,11 +49,18 @@ app.get('/video', async (req, res) => {
         'Content-length': contentLength,
         'Content-type': 'video/mp4'
     }
-
     res.writeHead(206, headers)
-
     const videoStream = videoFile.createReadStream({ start, end })
     videoStream.pipe(res)
+})
+
+app.get('/executeOCR', (req, res) => {
+    const execSync = require('child_process').execSync;
+    execSync('python ./../OCR/src/main.py');
+    const path = 'errors.txt'
+    try {fs.unlinkSync(path)}
+    catch(err) {console.error(err)}
+    res.sendStatus(200)
 })
 
 
