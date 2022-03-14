25cce309f183e03669069079ba90a71a55b73b37
noam-roth <73580442+noam-roth@users.noreply.github.com>
2022-03-14 18:11:27 +0200

    slidebar to Slidebar

---
+++ client/src/components/Sidebar/InnerMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SubMenu, Menu } from "react-pro-sidebar";
+import { Icon } from './Item'
+
+export default function InnerMenu({ children, title, icon, onClick, open }) {
+    if (!icon) icon = 'bi-border'
+    return (
+        <SubMenu title={title} icon={<Icon iconClass={`bi ${icon}`}/>} onClick={onClick} open={open}>
+            <Menu style={{ borderLeft: '2px solid lightgray' , marginTop: -20 }}>
+                {children}
+            </Menu>
+        </SubMenu>
+    )
+}
---
+++ client/src/components/Sidebar/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Menu as DefaultMenu, MenuItem } from 'react-pro-sidebar'
+import './Sidebar.scss'
+
+
+export default function Menu({ children, title }) {
+    return (
+        <DefaultMenu>
+            {!title ? <></> : <MenuItem className={'mt-2 title'}>{title}</MenuItem>}
+            <DefaultMenu className={'p-2 pt-0'}>{children}</DefaultMenu>
+        </DefaultMenu>
+    )
+}
