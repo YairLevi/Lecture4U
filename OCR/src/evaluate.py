1cb449cef0557bd6c55f96edf0322626a351fb4b
noam-roth <73580442+noam-roth@users.noreply.github.com>
2022-05-02 12:11:54 +0300

    detect line's order

--- OCR/src/evaluate.py
+++ OCR/src/evaluate.py
@@ -19,18 +19,6 @@ def evaluate_line(user_data, text_data, u_len, t_len):
     return counter / (min_len + abs(u_len - t_len))
 
 
-# splits the sentence into lowered words and compares their parsing - forward and backward alphabetically
-def evaluate_words(user_data, text_data, u_len, t_len):
-    counter1 = counter2 = 0
-    user_d, text_d = sorted(user_data), sorted(text_data)
-    min_len = min(len(user_d), len(text_d))
-    for i in range(min_len):
-        counter1 = counter1 + 1 if user_d[i] == text_d[i] else counter1
-    for i in range(min_len):
-        counter2 = counter2 + 1 if user_d[len(user_d) - i - 1] == text_d[len(text_d) - i - 1] else counter2
-    return max(counter1, counter2) / (min_len + abs(u_len - t_len))
-
-
 # merges all chars in the sentence into lowered stream and compares their similarity
 def evaluate_stream(user_data, text_data):
     counter = 0
@@ -51,13 +39,11 @@ def read_data(input_file):
 
 # geometric average of all 3 evaluations equally in total evaluation: algebraic average of the geometric average of
 # each line. each other semi-evaluation uses algebraic average of the lines
-def write_evaluation(text_data, evaluation, lines_eval, words_eval, streams_eval, values, lens):
+def write_evaluation(text_data, evaluation, lines_eval, streams_eval, values, lens):
     eval_file = open(evaluation, "w+")
     eval_file.write(text_data + "\n\n\n*****************************************************************************\n")
     line_eval = str(round(sum(lines_eval) / len(lines_eval), 8))
     eval_file.write("evaluation of words in order (compared to original text) = " + line_eval + "\n")
-    word_eval = str(round(sum(words_eval) / len(words_eval), 8))
-    eval_file.write("evaluation of words (compared to the words in the original text) = " + word_eval + "\n")
     stream_eval = str(round(sum(streams_eval) / len(streams_eval), 8))
     eval_file.write("evaluation of chars' stream in order (compared to original text) = " + stream_eval + "\n")
     total_eval = str(round(sum(values) / sum(lens), 8))
@@ -78,7 +64,7 @@ def process_data(user_data, text_data):
 # main evaluation function
 def evaluate(text, evaluation, input_file):
     user, data = read_data(input_file), read_data(text)
-    values, lines_eval, words_eval, streams_eval, lens = [], [], [], [], []
+    values, lines_eval, streams_eval, lens = [], [], [], []
     idx = 0
     for line in data:
         line = line.rstrip() if line == data[-1] else line
@@ -87,8 +73,7 @@ def evaluate(text, evaluation, input_file):
         lens.append(max_len)
         u_len, t_len, u_data, t_data = process_data(user_data, text_data)
         lines_eval.append(evaluate_line(u_data, t_data, u_len, t_len))
-        words_eval.append(evaluate_words(u_data, t_data, u_len, t_len))
         streams_eval.append(evaluate_stream(user_data, text_data))
-        values.append(max_len * (lines_eval[-1] + 2 * words_eval[-1] + 5 * streams_eval[-1]) / 8)
+        values.append(max_len * (3 * lines_eval[-1] + 5 * streams_eval[-1]) / 8)
         idx += 1
-    return write_evaluation(''.join(data).rstrip(), evaluation, lines_eval, words_eval, streams_eval, values, lens)
+    return write_evaluation(''.join(data).rstrip(), evaluation, lines_eval, streams_eval, values, lens)
--- OCR/src/transcription_model.py
+++ OCR/src/transcription_model.py
@@ -25,7 +25,7 @@ def ignore_warnings():
 
 
 # creates new images from a given image (text --> lines; lines --> words) into ROIs\ROIw folders
-def split_image_text(image_name, target_dir, kernel_size):
+def split_image_text(image_name, target_dir, kernel_size, is_line):
     img = cv2.imread(image_name, 1)
     # finds the optimal threshold to the image, which is called Otsu threshold (optimal k-means for back & foreground).
     # for more information on Otsu binarization: https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html
@@ -38,18 +38,38 @@ def split_image_text(image_name, target_dir, kernel_size):
     copied = img.copy()
     idx = len(contours)
     height_threshold = width_threshold = 10
-    for c in contours:
-        x, y, w, h = cv2.boundingRect(c)
+    if not is_line:
+        my_list = []
+        for c in contours:
+            x, y, w, h = cv2.boundingRect(c)
+            ROI = img[y:y + h, x:x + w]
+            # checks whether the size of the ROI is too small --> indicates there is no additional text in the rectangle
+            threshold_smaller = ROI.shape[0] < height_threshold or ROI.shape[1] < width_threshold
+            if threshold_smaller or ((ROI.shape[0] < 2 * height_threshold) and (ROI.shape[1] < 2 * width_threshold)):
+                continue
+            my_list.append((x, y, w, h))
+        my_list.sort(key=lambda z: z[0])
+    else:
+        my_list = []
+        for c in contours:
+            x, y, w, h = cv2.boundingRect(c)
+            ROI = img[y:y + h, x:x + w]
+            # checks whether the size of the ROI is too small --> indicates there is no additional text in the rectangle
+            threshold_smaller = ROI.shape[0] < height_threshold or ROI.shape[1] < width_threshold
+            if threshold_smaller or ((ROI.shape[0] < 2 * height_threshold) and (ROI.shape[1] < 2 * width_threshold)):
+                continue
+            my_list.append((x, y, w, h))
+    for i, c in enumerate(my_list):
+        x, y, w, h = c[0], c[1], c[2], c[3]
         ROI = img[y:y + h, x:x + w]
-        # checks whether the size of the ROI is too small --> indicates there is no additional text in the rectangle
-        threshold_smaller = ROI.shape[0] < height_threshold or ROI.shape[1] < width_threshold
-        if threshold_smaller or ((ROI.shape[0] < 2 * height_threshold) and (ROI.shape[1] < 2 * width_threshold)):
-            continue
         cv2.rectangle(copied, (x, y), (x + w, y + h), (36, 255, 12), 2)
         idx -= 1
         # converts the image to black and white
         r, t = cv2.threshold(cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-        cv2.imwrite(target_dir + '\\ROI_{:02d}.png'.format(idx), 255 - cv2.cvtColor(t, cv2.COLOR_BGR2RGB))
+        if is_line:
+            cv2.imwrite(target_dir + '\\ROI_{:02d}.png'.format(idx), 255 - cv2.cvtColor(t, cv2.COLOR_BGR2RGB))
+        else:
+            cv2.imwrite(target_dir + '\\ROI_{:04d}.png'.format(x), 255 - cv2.cvtColor(t, cv2.COLOR_BGR2RGB))
     return cv2.cvtColor(copied, cv2.COLOR_BGR2RGB)
 
 
@@ -98,21 +118,17 @@ def remove_irrelevant_images(p):
 
 # for each line, it parses the image into words and transcripts it into words and writes to the file
 def handle_htr(text, words_split_const):
-    lines, l_idx = glob.glob(LINES_ROI_PNG), 0               # 18: 58 -> 0.48, *59 -> 0.74, 60 -> 0.48, 61 -> 0.48
-    for img in lines:                                        # 17: 58 -> 0.43, *59 -> 0.43, 60 -> 0.43, 61 -> 0.49
-        split_image_text(img, WORDS_ROI, words_split_const)  # 14: 58 -> 0.48, *59 -> 0.50, 60 -> 0.48, 61 -> 0.53
-        current_line = glob.glob(WORDS_ROI_PNG)              # 16: 58 -> 0.74, *59 -> 0.74, 60 -> 0.74, 61 -> 0.54
-        if (l_idx != (len(lines) - 1)) or len(lines) == 1:
-            for img in current_line:
-                htr(img, text)
-            if len(lines) > 1:
-                out_text = open(text, "a+")  # adds new line (\n) to the end of the line
-                out_text.write("\n")
-                out_text.close()
-            l_idx += 1
-        else:
-            for img in current_line[::-1]:
-                htr(img, text)
+    lines, l_idx = glob.glob(LINES_ROI_PNG), 0
+    for img in lines:
+        split_image_text(img, WORDS_ROI, words_split_const, False)
+        current_line = glob.glob(WORDS_ROI_PNG)
+        for img in current_line:
+            htr(img, text)
+        if len(lines) > 1:
+            out_text = open(text, "a+")  # adds new line (\n) to the end of the line
+            out_text.write("\n")
+            out_text.close()
+        l_idx += 1
         remove_irrelevant_images(WORDS_ROI_PNG)
     remove_irrelevant_images(LINES_ROI_PNG)
 
@@ -122,10 +138,10 @@ def transcript(to_eval, text, words_split_const, to_detect):
     old_std = ignore_warnings()
     evaluation, filename = init_htr(to_eval, text)
     if to_detect:
-        cv2.imwrite('init_image.png', split_image_text("..\\image examples\\" + filename, LINES_ROI, words_split_const))
+        cv2.imwrite('init_image.png', split_image_text("..\\image examples\\" + filename, LINES_ROI, words_split_const, True))
         temp_folders()
     else:
-        split_image_text("..\\image examples\\" + filename, LINES_ROI, 150)
+        split_image_text("..\\image examples\\" + filename, LINES_ROI, 150, True)
         handle_htr(text, words_split_const)
         sys.stdout = old_std
         data = write_to_word(text)
