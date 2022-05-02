0f2f8e9d1a2577a02cb244b0e57c20895c4a00d3
noam-roth <73580442+noam-roth@users.noreply.github.com>
2022-05-02 14:14:34 +0300

    removed duplicates

--- OCR/src/transcription_model.py
+++ OCR/src/transcription_model.py
@@ -24,6 +24,19 @@ def ignore_warnings():
     return old_stdout
 
 
+def get_image_param(contours, height_threshold, width_threshold, img):
+    my_list = []
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        ROI = img[y:y + h, x:x + w]
+        # checks whether the size of the ROI is too small --> indicates there is no additional text in the rectangle
+        threshold_smaller = ROI.shape[0] < height_threshold or ROI.shape[1] < width_threshold
+        if threshold_smaller or ((ROI.shape[0] < 2 * height_threshold) and (ROI.shape[1] < 2 * width_threshold)):
+            continue
+        my_list.append((x, y, w, h))
+    return my_list
+
+
 # creates new images from a given image (text --> lines; lines --> words) into ROIs\ROIw folders
 def split_image_text(image_name, target_dir, kernel_size, is_line):
     img = cv2.imread(image_name, 1)
@@ -37,37 +50,18 @@ def split_image_text(image_name, target_dir, kernel_size, is_line):
     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     copied = img.copy()
     idx = len(contours)
-    height_threshold = width_threshold = 10
+    h_thresh = w_thresh = 10
+    my_list = get_image_param(contours, h_thresh, w_thresh, img)
     if not is_line:
-        my_list = []
-        for c in contours:
-            x, y, w, h = cv2.boundingRect(c)
-            ROI = img[y:y + h, x:x + w]
-            # checks whether the size of the ROI is too small --> indicates there is no additional text in the rectangle
-            threshold_smaller = ROI.shape[0] < height_threshold or ROI.shape[1] < width_threshold
-            if threshold_smaller or ((ROI.shape[0] < 2 * height_threshold) and (ROI.shape[1] < 2 * width_threshold)):
-                continue
-            my_list.append((x, y, w, h))
         my_list.sort(key=lambda z: z[0])
-    else:
-        my_list = []
-        for c in contours:
-            x, y, w, h = cv2.boundingRect(c)
-            ROI = img[y:y + h, x:x + w]
-            # checks whether the size of the ROI is too small --> indicates there is no additional text in the rectangle
-            threshold_smaller = ROI.shape[0] < height_threshold or ROI.shape[1] < width_threshold
-            if threshold_smaller or ((ROI.shape[0] < 2 * height_threshold) and (ROI.shape[1] < 2 * width_threshold)):
-                continue
-            my_list.append((x, y, w, h))
     for i, c in enumerate(my_list):
         x, y, w, h = c[0], c[1], c[2], c[3]
-        ROI = img[y:y + h, x:x + w]
         cv2.rectangle(copied, (x, y), (x + w, y + h), (36, 255, 12), 2)
-        idx -= 1
         # converts the image to black and white
-        r, t = cv2.threshold(cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+        r, t = cv2.threshold(cv2.cvtColor(img[y:y + h, x:x + w], cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
         if is_line:
             cv2.imwrite(target_dir + '\\ROI_{:02d}.png'.format(idx), 255 - cv2.cvtColor(t, cv2.COLOR_BGR2RGB))
+            idx -= 1
         else:
             cv2.imwrite(target_dir + '\\ROI_{:04d}.png'.format(x), 255 - cv2.cvtColor(t, cv2.COLOR_BGR2RGB))
     return cv2.cvtColor(copied, cv2.COLOR_BGR2RGB)
@@ -134,15 +128,15 @@ def handle_htr(text, words_split_const):
 
 
 # main transcription function
-def transcript(to_eval, text, words_split_const, to_detect):
+def transcript(to_eval, text, words_split, to_detect):
     old_std = ignore_warnings()
     evaluation, filename = init_htr(to_eval, text)
     if to_detect:
-        cv2.imwrite('init_image.png', split_image_text("..\\image examples\\" + filename, LINES_ROI, words_split_const, True))
+        cv2.imwrite('init_image.png', split_image_text("..\\image examples\\" + filename, LINES_ROI, words_split, True))
         temp_folders()
     else:
         split_image_text("..\\image examples\\" + filename, LINES_ROI, 150, True)
-        handle_htr(text, words_split_const)
+        handle_htr(text, words_split)
         sys.stdout = old_std
         data = write_to_word(text)
         return evaluate(text, evaluation, "..\\evaluation input\\input.txt") if to_eval else data
