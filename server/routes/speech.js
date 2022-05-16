c7c2b6f012e68c85eb3d7a0b881965146aba0f3a
YairLevi <79797419+YairLevi@users.noreply.github.com>
2022-05-16 15:48:16 +0300

    API routes for saving speech-to-text timestamps

--- server/routes/speech.js
+++ server/routes/speech.js
@@ -3,12 +3,30 @@ const express = require('express')
 const axios = require('axios')
 const router = express.Router()
 
+const Timestamp = require('../models/Timestamp')
+const { getUserID } = require("../httpUtil");
 
-router.get('/transcribe_score', async (req, res) => {
-    const response = await axios.get('http://localhost:5000/transcribe_score', req)
-    console.log(response)
-    res.json(response)
+router.post('/save', async (req, res) => {
+    const data = req.body.data
+    const userId = getUserID(req)
+    const doesExist = await Timestamp.exists({ userId })
+    if (doesExist) {
+        await Timestamp.findOneAndUpdate( { userId }, { data })
+    } else {
+        await Timestamp.create({ data, userId})
+    }
+    res.sendStatus(200)
+})
+
+router.get('/get', async (req, res) => {
+    const userId = getUserID(req)
+    let data = []
+    const timestamp = await Timestamp.findOne({ userId })
+    if (timestamp) data = timestamp.data
+    res.status(200).json(data)
 })
 
 
 module.exports = router
+
+
