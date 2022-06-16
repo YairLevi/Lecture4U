cf9d38d710025eb61853a7378c730bb99abea124
YairLevi <79797419+YairLevi@users.noreply.github.com>
2022-06-16 17:22:46 +0300

    AuthContext using requests.js instaed of fetch

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -12,22 +12,14 @@ export default function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState(null)
 
     const loading = useLoadingEffect(async () => {
-        const options = { credentials: 'include' }
-        const result = await fetch('http://localhost:8000/login', options)
+        const result = await requests.get('/login')
         if (result.status !== 200) return
         const user = await result.json()
         setCurrentUser(user)
     }, [])
 
     async function login(email, password) {
-        const data = { email, password }
-        const options = {
-            method: 'POST',
-            headers: { 'Content-type': 'application/json', },
-            body: JSON.stringify(data),
-            credentials: 'include'
-        }
-        const result = await fetch('http://localhost:8000/login', options)
+        const result = await requests.post('/login', { email, password })
         if (result.status !== 200) return false
         const user = await result.json()
         setCurrentUser(user)
@@ -35,18 +27,12 @@ export default function AuthProvider({ children }) {
     }
 
     async function register(data) {
-        const options = {
-            method: 'POST',
-            headers: { 'Content-type': 'application/json' },
-            body: JSON.stringify(data),
-            credentials: 'include',
-        }
-        const result = await fetch('http://localhost:8000/register', options)
+        const result = await requests.post('/register', data)
         return result.status === 200
     }
 
     async function logout() {
-        const result = await fetch('http://localhost:8000/logout', { credentials: 'include' })
+        const result = await requests.get('/logout')
         if (result.status === 200)
             setCurrentUser(null)
     }
