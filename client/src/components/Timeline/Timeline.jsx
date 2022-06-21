ae315736c7023a155e24783396db1bbe7bec3d7a
YairLevi <79797419+YairLevi@users.noreply.github.com>
2022-06-21 13:52:35 +0300

    fix timeline 'nothing happened yet'
    - when events is empty, dont show anything related to the ui of the timeline.
    the problem was it showed a beginning of a timeline with the text behind it, instead of only the text

--- client/src/components/Timeline/Timeline.jsx
+++ client/src/components/Timeline/Timeline.jsx
@@ -8,19 +8,21 @@ export default function Timeline({ events }) {
     return (
         <Card className="main-card mb-3 overflow-auto" style={{ height: '350px' }}>
             <div className="card-body overflow-auto">
-                <div className="vertical-timeline vertical-timeline--animate vertical-timeline--one-column">
-                    {
-                        events.length === 0 ?
-                            <p>Nothing happened yet.</p> :
-                            events.slice(0).reverse().map((value, index) => {
-                                const time = new Date(value.createdAt).parseEventDate()
-                                const color = eventSettings[value.title].color
-                                const text = eventSettings[value.title].text.format(...value.args)
-                                const title = value.title.replace(new RegExp("_", "g"), ' ')
-                                return <TimelineItem key={index} {...{ time, color, text, title }}/>
-                            })
-                    }
-                </div>
+                {
+                    events.length === 0 ?
+                        <p>Nothing happened yet.</p> :
+                        <div className="vertical-timeline vertical-timeline--animate vertical-timeline--one-column">
+                            {
+                                events.slice(0).reverse().map((value, index) => {
+                                    const time = new Date(value.createdAt).parseEventDate()
+                                    const color = eventSettings[value.title].color
+                                    const text = eventSettings[value.title].text.format(...value.args)
+                                    const title = value.title.replace(new RegExp("_", "g"), ' ')
+                                    return <TimelineItem key={index} {...{ time, color, text, title }}/>
+                                })
+                            }
+                        </div>
+                }
             </div>
         </Card>
     )
