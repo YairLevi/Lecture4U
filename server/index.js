901cf743f17568c8029ae648b63f42749a1ed87c
YairLevi <79797419+YairLevi@users.noreply.github.com>
2022-07-25 09:23:02 +0300

    cleanup

--- server/index.js
+++ server/index.js
@@ -1,15 +1,11 @@
+// imports
 require('dotenv').config({ path: './.env' })
 const express = require("express")
-const multer = require('multer')
 const cors = require('cors')
-const User = require("./models/User");
-const jwt = require("jsonwebtoken");
 const cookieParser = require('cookie-parser')
-const fs = require('fs')
-const path = require("path");
 const mongoose = require("mongoose");
-mongoose.connect(process.env.URL)
 
+// routers defined in ./routes
 const authRouter = require('./routes/auth')
 const courseRouter = require('./routes/course')
 const speechRouter = require('./routes/speech')
@@ -21,11 +17,16 @@ const dashboardRouter = require('./routes/dashboard')
 const ocrRouter = require('./routes/ocr')
 const mailRouter = require('./routes/mail')
 
+// connect to mongodb
+mongoose.connect(process.env.URL)
+
+// run server
 const PORT = process.env.SERVER_PORT
 const HOST = process.env.SERVER_ADDR
 const app = express()
 const server = require('http').Server(app)
 
+// use routers and middleware (cors)
 app.use(cors({ origin: `http://${process.env.CLIENT_ADDR}:${process.env.CLIENT_PORT}`, credentials: true, }))
 app.use(cookieParser())
 app.use(express.json())
@@ -40,6 +41,45 @@ app.use('/profile', profileRouter)
 app.use('/ocr', ocrRouter)
 app.use('/mail', mailRouter)
 
+// listen on port
+server.listen(PORT, HOST, callback = () => console.log(`server started on port ${PORT}`))
+// app.listen(PORT, HOST, () => console.log('test here!!!'))
+
+const Document = require("./models/Document")
+
+// create io
+const io = require("socket.io")(server, {
+    cors: {
+        origin: `http://${process.env.CLIENT_ADDR}:${process.env.CLIENT_PORT}`,
+        methods: ['GET', 'POST']
+    }
+})
+
+// for faster receive
+const doc_dict = {}
+
+// io connections
+io.on("connection", socket => {
+    socket.on("get-document", async documentId => {
+        const document = await Document.findById(documentId)
+        socket.join(documentId)
+        socket.emit("load-document", document.data)
+        doc_dict[documentId] = document.data
+
+        socket.on("send-changes", delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta)
+        })
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentId, { data })
+        })
+    })
+})
+
+
+
+
+// Helper prototypes
+
 Date.prototype.addHours = function (h) {
     this.setTime(this.getTime() + (h * 60 * 60 * 1000));
     return this;
@@ -50,42 +90,3 @@ Date.prototype.getMonthAndDay = function () {
     const month = this.toLocaleString('default', { month: 'short' })
     return `${month} ${day}`
 }
-
-
-server.listen(PORT, HOST, callback = () => console.log(`server started on port ${PORT}`))
-// app.listen(PORT, HOST, () => console.log('test here!!!'))
-
-const Document = require("./models/Document")
-
-
-const io = require("socket.io")(server, {
-    cors: {
-        origin: `http://${process.env.CLIENT_ADDR}:${process.env.CLIENT_PORT}`,
-        methods: ['GET', 'POST']
-    }
-})
-
-const defaultValue = ""
-
-const doc_dict = {}
-
-io.on("connection", socket => {
-    socket.on("get-document", async documentId => {
-
-        // needs to use DB for returning the same documentId when in same group (using groupId?)
-
-        const document = await Document.findById(documentId)
-        socket.join(documentId)
-        socket.emit("load-document", document.data)
-
-        doc_dict[documentId] = document.data
-        // console.log(doc_dict)
-
-        socket.on("send-changes", delta => {
-            socket.broadcast.to(documentId).emit("receive-changes", delta)
-        })
-        socket.on("save-document", async data => {
-            await Document.findByIdAndUpdate(documentId, { data })
-        })
-    })
-})
